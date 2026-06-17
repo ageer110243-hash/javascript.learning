f82fa0556eb63d7909e28d6e03764f133878967a
ageer110243-hash <ageer110243@gmail.com>
2026-06-17 15:06:42 +0500

    variables

---
+++ varaible.js
@@ -0,0 +1,25 @@
+const accountId = 144234
+let acccountEmail = "email@gmail.com"
+var accountPassword = "alamg23"
+accountCity = "hyd"
+
+
+// accountId = 2  not allowed
+
+console.log(accountId)
+
+/*
+
+prefer not to use var
+because of issue in block scop and functional scop
+
+
+*/
+
+
+acccountEmail = "gmail@gmail.com"
+accountPassword = "113144"
+accountCity = "karachi"
+
+
+console.table([accountId, acccountEmail, accountPassword, accountCity])
