4142ce2982fd21bcc962d3385763aeabad3f1538
ageer110243-hash <ageer110243@gmail.com>
2026-06-17 15:50:03 +0500

    strings

---
+++ strings.js
@@ -0,0 +1,25 @@
+const name = "alamgeer"
+const repocount  = 40
+
+// // console.log(nama + repcount + "alam") 
+// console.log(` My Name is ${name} and my repo count is ${repocount}`)
+
+console.log(name.charAt(2))
+console.log(name.indexOf("e"))
+
+console.log(name.substring(0 , 4))
+console.log(name.slice(0, 4))
+
+const newSrting = name.toUpperCase()
+console.log(newSrting)
+
+const another = "   alam  "
+console.log(another)
+console.log(another.trim())
+
+
+const url = "https://alam.com/50%geer"
+console.log(url.replace("50%", "_"))
+console.log(url.includes("alam"))
+ 
+
