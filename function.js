d7129087394291bf464b27f3c159ee613b17204f
ageer110243-hash <ageer110243@gmail.com>
2026-06-18 15:31:32 +0500

    function

---
+++ function.js
@@ -0,0 +1,39 @@
+// function myName() {
+//     console.log("a")
+//     console.log("l")
+//     console.log("a")
+//     console.log("m")
+// }
+
+// myName()
+
+// function sumNumber(num1, num2){
+//    console.log( num1 + num2)
+
+
+// }
+// sumNumber(3, 5)
+
+// function addNumber(num1, num2) {
+
+//    return num1 + num2
+
+
+// }
+
+// const addition = addNumber(3, 5)
+// console.log( "result", addition)
+
+// function myName(userName){
+
+//     if(userName === undefined){
+//         return "please Loged in"
+//     }
+
+//    return `${userName } Just Logged in`
+
+
+// }
+
+//  const result = myName()
+//  console.log(result)
