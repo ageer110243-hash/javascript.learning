86f4de452fb12fd79877fd254d0ea2d61246ab7b
ageer110243-hash <ageer110243@gmail.com>
2026-06-18 16:03:55 +0500

    arrow function

---
+++ arrow f .js
@@ -0,0 +1,23 @@
+// const  user = {
+//     username : "alam",
+//     price : 1000,
+//     welcomeMessage : function(){
+    
+//         console.log(`${this.username}, welcome to website`)
+//     }
+
+// }
+
+// user.welcomeMessage()
+// user.username = "geer"
+// user.welcomeMessage()
+ 
+//   const addTwo = (num1, num2) =>{
+//     return num1 + num2
+
+//   }
+
+//   console.log(addTwo(2, 5))
+
+const addTwo = ( num1, num2) => num1 + num2
+ console.log(addTwo(2, 5))
