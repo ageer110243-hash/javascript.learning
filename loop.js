94463d3401d560473c98c49766ce617ca9ce8db1
ageer110243-hash <ageer110243@gmail.com>
2026-06-19 15:58:53 +0500

    loop

---
+++ loop.js
@@ -0,0 +1,55 @@
+// // for loop
+// for(let i = 0; i<= 10; i++){
+//     const element = i
+//     console.log(element)
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i ;
+    
+//     console.log(`outerloop : ${i}`)
+
+//     for (let j = 0; j <= 10; j++) {
+//         const element = i;
+
+//         console.log(`inner loop : ${j} and outerloop ${i}`) 
+        
+//     }
+
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+
+//     console.log(`table of ${i}`)
+
+//     for (let j = 0; j <=10; j++) {
+//         const element = j;
+//         console.log(`${i} x ${j} = ${j*i}`)
+//     }
+
+
+    
+// }
+
+// while loop
+
+
+// // let index = 0
+// while (index <= 10) {
+
+//     console.log(index)
+//      index = index + 2
+    
+// }
+
+
+//  Do while loop
+
+let score =  1
+
+do {
+    console.log(score);
+    score++;
+} while( score <= 10)
