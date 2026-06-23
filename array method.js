fbf56be15d47913826aacc3de521de24df2a475e
ageer110243-hash <ageer110243@gmail.com>
2026-06-23 16:26:50 +0500

    array m

--- array method.js
+++ array method.js
@@ -1,69 +1,102 @@
-const fruits = ["apple", "banana", "orange"]
+// const fruits = ["apple", "banana", "orange"]
 
  
-// length 
+// // length 
 
-const arr = [10, 20, 30, 40]
-console.log(arr.length)
+// const arr = [10, 20, 30, 40]
+// console.log(arr.length)
    
 
 
-// at 
+// // at 
 
-console.log(arr.at(1))
-console.log(arr.at(-1))
+// console.log(arr.at(1))
+// console.log(arr.at(-1))
 
-// push
+// // push
 
-arr.push(50)
-console.log(arr)
+// arr.push(50)
+// console.log(arr)
 
-// pop 
+// // pop 
 
-arr.pop()
-console.log(arr)
+// arr.pop()
+// console.log(arr)
 
-// shift
+// // shift
 
-arr.shift()
-console.log(arr)
+// arr.shift()
+// console.log(arr)
 
-// unshift
+// // unshift
 
-arr.unshift(30)
-console.log(arr)
+// arr.unshift(30)
+// console.log(arr)
 
-// concat
+// // concat
 
-const arr1 = [1,2,3,4]
-const arr2 = [5,6,7,8]
-const arr3 = arr1.concat(arr2)
-console.log(arr3)
+// const arr1 = [1,2,3,4]
+// const arr2 = [5,6,7,8]
+// const arr3 = arr1.concat(arr2)
+// console.log(arr3)
 
-// slice
+// // slice
 
-console.log(arr1.slice(1,))
+// console.log(arr1.slice(1,))
 
-// toString
+// // toString
 
-console.log(arr1.toString())
+// console.log(arr1.toString())
 
-// splice
- arr1.splice(2, 0, 45, 50)
- console.log(arr1)
+// // splice
+//  arr1.splice(2, 0, 45, 50)
+//  console.log(arr1)
 
- //Includes
+//  //Includes
 
- console.log(arr1.includes(1))
+//  console.log(arr1.includes(1))
 
- //reverse
+//  //reverse
 
- arr1.reverse();
- console.log(arr1)
+//  arr1.reverse();
+//  console.log(arr1)
 
- //sort
+//  //sort
 
- arr1.sort()
- console.log(arr1)
+//  arr1.sort()
+//  console.log(arr1)
 
- 
+
+
+// const digits = [8, 6, 9, 4, 23, 7, 5, 8]
+
+// const cube = digits.map(digit => digit ** 3)
+// console.log(cube)
+
+// const fillterD = cube.filter(digit => digit >350).map(digitf =>  digitf -= 7)
+// console.log(fillterD)
+
+// const total = fillterD.reduce((acc, val) => acc + val, 0)
+// console.log(total)
+
+// const students = [
+//     {
+//         name : "abc",
+//         marks : 89
+//     },
+//      {
+//         name : "ac",
+//         marks : 49
+//     },
+//      {
+//         name : "ab",
+//         marks : 78
+//     }
+// ]
+
+// const total = students.reduce((acc, val) => acc + val.marks, 0)
+// console.log(total)
+
+// const avg = total/students.length
+// console.log(avg)
+
