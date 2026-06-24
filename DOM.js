49bf32a34a4c3c8392090bc178869e55cded1ef3
ageer110243-hash <ageer110243@gmail.com>
2026-06-24 16:29:13 +0500

    dom

---
+++ DOM.js
@@ -0,0 +1,65 @@
+//  const heading = document.getElementById("head")
+//  heading.innerText = "I am learning JS and HTML with DOM"
+//  heading.style.color = "blue"
+//  heading.style.backgroundColor = "yellow"
+//  heading.style.fontSize = "50px"
+//  console.log(heading)
+
+
+// const heading = document.getElementsByClassName("heading1")
+//   heading[0].innerText = "I am learning JS and HTML with DOM"
+//  heading[0].style.color = "blue"
+//  heading[0].style.backgroundColor = "yellow"
+//  heading[0].style.fontSize = "50px"
+//  console[0].log(heading)
+
+// const heading = document.querySelector(".heading1");
+//  heading.innerText = "I am learning JS and HTML with DOM"
+//  heading.style.color = "blue"
+//  heading.style.backgroundColor = "yellow"
+//  heading.style.fontSize = "50px"
+//  console.log(heading)
+
+
+const heading = document.querySelector(".heading1")
+const red = document.querySelector(".red")
+const green = document.querySelector(".green")
+
+
+red.addEventListener("click", ()=>{
+    heading.innerText = "i am red"
+    heading.style.color = "red"
+})
+
+
+const changeGreenColor = ()=>{
+    heading.innerText = "i am green"
+    heading.style.color = "green"
+}
+green.addEventListener("click", changeGreenColor)
+
+const body = document.querySelector("body")
+
+const changeBodyColor = ()=> {
+    let red = Math.floor(Math.random()* 255)
+    let blue = Math.floor(Math.random()* 255)
+    let green = Math.floor(Math.random()* 255)
+    console.log(red, blue, green)
+
+    let color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color
+}
+
+
+
+const bodyC = document.querySelector(".bodyC")
+bodyC.addEventListener("click", ()=>{
+    changeBodyColor()
+})
+
+    
+
+
+
+
+
